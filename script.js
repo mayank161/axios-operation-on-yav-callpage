8c38a03834640d7a6cbed1fcb8db6c5c21b020a6
Mayank <satimayank74@gmail.com>
2021-04-01 04:36:23 -0700

    getfrom axios

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 let button = document.getElementById('button');
 
 button.onclick = () => {
-    let Name = document.getElementById('name').value;
-    let Email = document.getElementById('email').value;
-    let Phone = document.getElementById('phone').value;
+    var Name = document.getElementById('name').value;
+    var Email = document.getElementById('email').value;
+    var Phone = document.getElementById('phone').value;
     console.log(Name);
     console.log(Email);
     console.log(Phone);
-    const dt =axios.post("https://crudcrud.com/api/346877b6e03449509343cf4a9ac7370c/yavCallPage",{
+    const post =axios.post("https://crudcrud.com/api/c9d03e1e122746f5a9157c18d552a3ec/yavCallPage",{
         Name,Email,Phone
-    }).then(data => console.log(data))
+    })
+    location.reload();
 }
+const get = axios.get("https://crudcrud.com/api/c9d03e1e122746f5a9157c18d552a3ec/yavCallPage")
+    .then(data => {
+        data.data.forEach(element => {
+            document.getElementById('namO').innerHTML += `${element.Name}<br>`
+            document.getElementById('emailO').innerHTML += `${element.Email}<br>`
+            document.getElementById('phonO').innerHTML += `${element.Phone}<br>`
+            document.getElementById('edit').innerHTM += `<button>EDIT</button> <br>`
+            document.getElementById('delete').innerHTM += `<button>DELETE</button> <br>`
+            console.log(element.Name)
+        });
+    })
